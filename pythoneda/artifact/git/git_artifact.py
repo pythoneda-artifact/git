2d491fc4e66b623b002b512b62adf2f89969437e
rydnr <github@acm-sl.org>
2024-01-17 19:10:52 +0100

    Added vim and emacs settings

--- pythoneda/artifact/git/git_artifact.py
+++ pythoneda/artifact/git/git_artifact.py
@@ -227,3 +227,11 @@ loop.run_until_complete(CodeRequest.main("{event.id}"))
         GitArtifact.logger().info(f"Emitting {result}")
         await Ports.instance().resolve(EventEmitter).emit(result)
         return result
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
